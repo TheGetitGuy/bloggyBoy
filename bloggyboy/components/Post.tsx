3484789e83d62a9b0692b558f9b92281c83b28fd
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-11-28 21:01:03 -0500

    styling and debugging

--- bloggyboy/components/Post.tsx
+++ bloggyboy/components/Post.tsx
@@ -14,16 +14,16 @@ const Post = ( props: Props ):JSX.Element => {
     const { content: contentParsed, data:dataParsed } = JSON.parse(content)
     console.log()
     return(
-        <div className='text-black w-full h-fit sm:w-1/2 md:w-96 relative border border-secondary shadow-md'>
-          <img src={dataParsed.image} className="aspect-video shadow-inner shadow-black" alt={dataParsed.title}></img>
-          <div className='flex justify-between flex-wrap  gap-2 p-2'>
+        <div className='even:rotate-1 odd:-rotate-1 text-black w-full sm:w-1/2 md:w-72 relative border border-secondary shadow-md'>
+          <img src={dataParsed.image} className="h-72 w-full object-cover shadow-inner shadow-black" alt={dataParsed.title}/>
+          <div className='flex justify-between flex-wrap  gap-2 m-4'>
             <div className='w-full h-24 text-ellipsis text-primaryLight overflow-hidden'>
                 <div className={styles.markdown}>
                 <Markdown>{contentParsed}</Markdown>
                 </div>
             </div>
             <Link href={`/posts/${id}`} className='h-fit'>
-                <div role="button" className=' bg-white rounded border-secondary text-center text-md hover:bg-secondary text-secondaryDark transition-all shadow-lg p-1 hover:shadow-x'>
+                <div role="button" className=' bg-white rounded border-secondary text-center text-md hover:bg-secondary text-secondaryDark transition-all shadow-md p-1 hover:shadow-xl'>
                 Read More
                     <Image className='inline h-8 ' src={arrowRSvg} alt="->"/>
                 </div>
